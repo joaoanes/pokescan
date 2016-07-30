92b62e6c7dfd377e0ca5b38f3100aef3deb60274
João Anes <joao.anes@gmail.com>
2016-07-30 21:23:26 +0100

    Activated changelog via cookie with funny name

--- platinum/routes/index.js
+++ platinum/routes/index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 
-var changelog = { "0.2" :
+var changelogs = [{ version: "0.2", changelog:
 [
 	"Added changelog and versioning!",
 	"Added throttles on updates on the mobile version",
 	"About that, did you know we were pulling 500kb each second? Yikes.",
 	""
-]}
+]}]
 
 router.get('/', function(req, res, next) {
-  res.render('index', { pageData: { changelog: changelog } });
+  res.render('index', { pageData: { changelogs: changelogs } });
 });
 
 module.exports = router;
